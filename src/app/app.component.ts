6ee369013d80d6ef270fb9e39592830bf73f3306
Krishna-sm <krishna.web18@gmail.com>
2023-06-26 23:00:43 +0530

    initalize app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'Krishna Website';
+ constructor(){
+  setTimeout(()=>{
+    this.title="My App";
+  },5000)
+ }
 }
